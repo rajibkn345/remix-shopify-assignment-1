75c8cc4eb7f1e69750d1897cdc3e3dee722a47a5
Roman Khan Rajib <rajibkn345@gmail.com>
2023-12-07 17:13:57 +0600

    updated

--- app/routes/app.collection.jsx
+++ app/routes/app.collection.jsx
@@ -5,29 +5,25 @@ import {
   Card,
   Frame,
   Modal,
-  TextContainer,
+  FormLayout,
+  TextField,
 } from "@shopify/polaris";
 import modalStyle from "../styles/modal.css";
 import { useState, useCallback, useRef } from "react";
 import { Link } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import { Form, useLoaderData } from "@remix-run/react";
-import CollectionForm from "../components/CollectionForm";
+import { Form, useLoaderData, useSubmit } from "@remix-run/react";
+import PopUpModal from "../components/PopUpModal";
 
 function collection() {
-  const [active, setActive] = useState(false);
+  const submit = useSubmit();
+  const [id, setId] = useState(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  const button = useRef();
+  const [showModal, setShowModal] = useState(false);
 
-  const handleOpen = useCallback(() => setActive(true), []);
-
-  const handleClose = useCallback(() => {
-    setActive(false);
-    requestAnimationFrame(() =>
-      button.current?.querySelector("button")?.focus()
-    );
-  }, []);
   const { collection } = useLoaderData();
 
   const itemCollections = collection.map((collection) => {
@@ -41,22 +37,51 @@ function collection() {
       <Text as="h2" alignment="center" variant="headingLg">
         Collections
       </Text>
-      <div style={{ margin: "20px 10px" }} ref={button}>
-        <Button onClick={handleOpen} tone="success">
+      <div style={{ margin: "20px 10px" }}>
+        <Button
+          onClick={() => {
+            setShowModal(true);
+            setId(null);
+          }}
+          tone="success"
+        >
           Add Collection
         </Button>
       </div>
       <div>
-        <Modal
-          instant
-          open={active}
-          onClose={handleClose}
-          title="Add Collections"
-        >
-          <Modal.Section>
-            <CollectionForm onClose={handleClose} />
-          </Modal.Section>
-        </Modal>
+        {showModal && (
+          <PopUpModal
+            title={id ? "Edit Collection" : "Create Collection"}
+            setShowModal={setShowModal}
+            onPrimaryAction={() => {
+              submit(
+                {
+                  id,
+                  title,
+                  description,
+                },
+                { replace: true, method: "POST" }
+              );
+            }}
+          >
+            <FormLayout>
+              <TextField
+                label="Title"
+                type="text"
+                value={title}
+                autoComplete="off"
+                onChange={setTitle}
+              />
+              <TextField
+                label="Description"
+                type="text"
+                value={description}
+                autoComplete="off"
+                onChange={setDescription}
+              />
+            </FormLayout>
+          </PopUpModal>
+        )}
       </div>
       <Card>
         <table>
@@ -85,7 +110,18 @@ function collection() {
                       width: "120px",
                     }}
                   >
-                    <Button tone="success">edit</Button>
+                    <Button
+                      onClick={() => {
+                        setId(item.id);
+                        setTitle(item.title);
+                        setDescription(item.description);
+                        setShowModal(true);
+                      }}
+                      tone="success"
+                    >
+                      edit
+                    </Button>
+
                     <Form method="DELETE">
                       <input
                         type="text"
@@ -142,13 +178,16 @@ export const links = () => [{ rel: "stylesheet", href: modalStyle }];
 
 export async function action({ request }) {
   const { admin } = await authenticate.admin(request);
+  console.log("method=======", request.method);
   if (request.method === "POST") {
     const formData = await request.formData();
     const inputData = Object.fromEntries(formData);
-    //   console.log("in======", inputData);
+    console.log("in======", inputData);
+    const id = inputData.id;
 
-    const response = await admin.graphql(
-      `#graphql
+    if (id == "null") {
+      const response = await admin.graphql(
+        `#graphql
         mutation CollectionCreate($input: CollectionInput!) {
           collectionCreate(input: $input) {
             userErrors {
@@ -172,27 +211,52 @@ export async function action({ request }) {
             }
           }
         }`,
-      {
-        variables: {
-          input: {
-            title: inputData.title,
-            descriptionHtml: inputData.description,
-            ruleSet: {
-              appliedDisjunctively: false,
-              rules: {
-                column: "TITLE",
-                relation: "CONTAINS",
-                condition: "shoe",
+        {
+          variables: {
+            input: {
+              title: inputData.title,
+              descriptionHtml: inputData.description,
+              ruleSet: {
+                appliedDisjunctively: false,
+                rules: {
+                  column: "TITLE",
+                  relation: "CONTAINS",
+                  condition: "shoe",
+                },
               },
             },
           },
-        },
-      }
-    );
+        }
+      );
 
-    const data = await response.json();
-    console.log("data=======", data);
-    return null;
+      const data = await response.json();
+      console.log("data=======", data);
+      return null;
+    } else {
+      console.log("id==========", id);
+      const response = await admin.graphql(
+        `#graphql
+        mutation updateCollection {
+          collectionUpdate(input: {id: "${inputData.id}",title:"${inputData.title}" ,descriptionHtml:"${inputData.description}"}) {
+            collection {
+              id
+              title
+              descriptionHtml
+              
+            }
+            userErrors {
+              field
+              message
+            }
+          }
+        }`
+      );
+
+      const data = await response.json();
+
+      console.log("update======", data.data.collectionUpdate.collection);
+      return null;
+    }
   } else if (request.method === "DELETE") {
     const formData = await request.formData();
     const delData = Object.fromEntries(formData);
@@ -221,6 +285,6 @@ export async function action({ request }) {
       }
     );
     const data = await response.json();
-    return { data };
+    return null;
   }
 }
